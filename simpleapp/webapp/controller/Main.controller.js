7cd98d73c27f58c573423c3fe05f0af596e82dd3
Andrey Yuryev <andrey.yuryev@t-systems.com>
2021-11-16 16:56:48 +0000

    ,

--- simpleapp/webapp/controller/Main.controller.js
+++ simpleapp/webapp/controller/Main.controller.js
@@ -1,15 +1,22 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller) {
+	function (Controller, MessageToast) {
 		"use strict";
 
 		return Controller.extend("simpleapp.controller.Main", {
 			onInit: function () {
 
-			}
+            },
+            
+            onButtonPressed: function (oEvent) {
+
+                let msg = oEvent.getSource().getProperty('text');
+                MessageToast.show("pressed button" + msg);
+            }
 		});
 	});
